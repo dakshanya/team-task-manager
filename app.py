2c472cd16d4b167e025a03fa8298114f4456610d
dakshanya <dakshanyasv@gmail.com>
2026-05-02 00:06:05 +0530

    portupdated - Team Task Manager full stack project

--- app.py
+++ app.py
@@ -525,6 +525,7 @@ def api_tasks():
 # ==================================================
 # RUN
 # ==================================================
+import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
